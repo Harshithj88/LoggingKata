e2173c3647c0e5fa7d449f0b222f4928eb648c4a
Harshith Julapelli <harsh.julapelli@gmail.com>
2017-10-31 09:37:23 -0500

    Completed Taco Parser Implementation

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -5,32 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using log4net;
 using System.IO;
+using Geolocation;
 
 namespace LoggingKata
 {
     class Program
     {
         //Why do you think we use ILog?
-        private static readonly ILog Logger =
+        private static readonly ILog Logger = 
             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            var path = Environment.CurrentDirectory + "Taco_Bell-US-AL-Alabama.csv";
+            if (path.Length == 0)
             {
                 Console.WriteLine("You must provide a filename as an argument");
                 Logger.Fatal("Cannot import without filename specified as an argument");
+                Console.ReadLine();
                 return;
             }
-
             Logger.Info("Log initialized");
-            var lines = File.ReadAllLines(args[0]);
+            Logger.Info("Grabbing form path:" +path);
+            var lines = File.ReadAllLines(path);
+            if (lines.Length == 0)
+            {
+                Logger.Error("No Locations to check. Must have atleast one Location");
+            }
+            else if (lines.Length == 1)
+            {
+                Logger.Warn("Only one Location is provided");
+            }
             var parser = new TacoParser();
+            Logger.Debug("Initialized our parser");
             var locations = lines.Select(line => parser.Parse(line));
-
-            //TODO:  Find the two TacoBells in Alabama that are the furthurest from one another.
-            //HINT:  You'll need two nested forloops
-
+            Console.ReadLine();
         }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using log4net;
+using log4net.Core;
 
 namespace LoggingKata
 {
@@ -9,18 +12,41 @@ namespace LoggingKata
     /// </summary>
     public class TacoParser
     {
-        public TacoParser()
-        {
-
-        }
-
         private static readonly ILog Logger =
             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public ITrackable Parse(string line)
         {
             //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+            var items = line.Split(',');
+            if (items.Length < 3)
+            {
+                Logger.Error("Must have atleast three items to parse into ITrackable");
+                return null;
+            }
+
+            double lon = 0;
+            double lat = 0;
+
+            try
+            {
+                Logger.Debug("Parsing Longitude");
+                lon = double.Parse(items[0]);
+
+                Logger.Debug("Parsing Latitude");
+                lat = double.Parse(items[1]);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Failed to parse the location");
+                Console.WriteLine(e);
+                return null;
+            }
+            return new TacoBell //object Initializer
+            {
+                Name = items[2],
+                Location = new Point (lat,lon)
+            };
         }
     }
 }
